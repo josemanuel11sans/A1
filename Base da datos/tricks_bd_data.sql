1ac9d3d89a187ac06c9cde465cbb993870c62241
josemanuel11sans <20233tn078@utez.edu.mx>
2024-06-27 21:49:17 -0600

    base de datos y datos de las tablas prinsipales solo para

---
+++ Base da datos/tricks_bd_data.sql
@@ -0,0 +1,7 @@
+-- datos de tipo de usuario
+INSERT INTO `tricks`.`tipousuario` (`id_rol`, `tpoUsuarioRol`) VALUES ('1', 'Administrador');
+INSERT INTO `tricks`.`tipousuario` (`id_rol`, `tpoUsuarioRol`) VALUES ('2', 'Docente');
+INSERT INTO `tricks`.`tipousuario` (`id_rol`, `tpoUsuarioRol`) VALUES ('3', 'Aspirante');
+-- Datos de la tabla estado
+INSERT INTO `tricks`.`estado` (`id_estado`, `estado`) VALUES ('1', 'Activo');
+INSERT INTO `tricks`.`estado` (`id_estado`, `estado`) VALUES ('2', 'NoActivo');
